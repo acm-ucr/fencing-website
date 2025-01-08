b3edd0b600e459d073af4ba919d0f717cf706e51
sunbrandon <sunabrandon@gmail.com>
2025-01-08 11:44:57 -0800

    Set up files for first day

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-const Home = () => {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
-    </div>
-  );
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ReactQueryClientProvider } from "@/utils/react-query";
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Fencing Website",
+  description: "The Offical Fencing Club at UCR",
 };
 
-export default Home;
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ReactQueryClientProvider>
+          <Navigation />
+          {children}
+        </ReactQueryClientProvider>
+        <Footer />
+      </body>
+    </html>
+  );
+}
