72df87a4dfae83c9e44103d2d8f7c8592db00bcb
av12122 <pv144155@gmail.com>
2025-01-11 13:41:16 -0800

    title component 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import About from "@/components/About";
 import Join from "@/components/Join";
 import Landing from "@/components/Landing";
 import What from "@/components/What";
+import Title from "@/components/Title";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Landing />
-      <About />
-      <What />
-      <Join />
+    <div className=") flex h-screen w-screen items-center justify-center bg-[#091422]">
+      {/* <Landing /> */}
+      <Title />
+      {/* <About /> */}
+      {/* <What /> */}
+      {/* <Join /> */}
     </div>
   );
 };
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Title = () => {
-  return <div>Title</div>;
+  return (
+    <h1
+      className="flex items-center justify-center bg-[#008CFF33] text-white"
+      style={{
+        border: "solid",
+        borderRadius: "10px",
+        height: "74px",
+        width: "510px",
+        borderColor: "#008CFF",
+        fontSize: "28px",
+        fontWeight: "700",
+        borderWidth: "3px",
+      }}
+    >
+      Types of Fencing Swords
+    </h1>
+  );
 };
 
 export default Title;
