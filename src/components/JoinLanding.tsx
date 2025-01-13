cbf5a951c233425d3b7ce8060cd2e77a4692c896
LawMot <molawr12@gmail.com>
2025-01-13 14:40:06 -0800

    Join Landing Component

---
+++ src/components/JoinLanding.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const JoinLanding = () => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-black p-10">
+      <div className="m-5 h-14 w-full rounded-lg bg-blue-500/60">
+        <div className="m-2 text-wrap text-center text-sm text-white">
+          We look forward to seeing you soon! We have practice every Monday and
+          Wednesday 8-10PM!
+        </div>
+      </div>
+
+      <div className="h-28 w-full rounded-lg bg-gray-400"></div>
+    </div>
+  );
+};
+
+export default JoinLanding;
