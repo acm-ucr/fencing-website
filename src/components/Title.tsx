0cb732f151ccb650f2b69d256e59498182a698fc
BenSan142 <pattyfstty@gmail.com>
2025-01-13 17:00:13 -0800

    Test Title Comp
    Testing out if Title component changes will appear in the local host.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Title = () => {
-  return <div>Title</div>;
+  return (
+    <h1
+      className="flex items-center justify-center bg-[#008CFF33] text-white"
+      style={{
+        border: "solid",
+        borderRadius: "10px",
+        height: "74px",
+        width: "510px",
+        borderColor: "#008CFF",
+        borderWidth: "3px",
+        fontSize: "28px",
+        fontWeight: "700",
+      }}
+    >
+      Meet the Board
+    </h1>
+  );
 };
 
 export default Title;
