e9253d15aa0b929a3123b6963d344c03c97e7014
av12122 <pv144155@gmail.com>
2025-01-13 19:41:41 -0800

    testing git

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,21 +2,12 @@ import React from "react";
 
 const Title = () => {
   return (
-    <h1
-      className="flex items-center justify-center bg-[#008CFF33] text-white"
-      style={{
-        border: "solid",
-        borderRadius: "10px",
-        height: "74px",
-        width: "510px",
-        borderColor: "#008CFF",
-        borderWidth: "3px",
-        fontSize: "28px",
-        fontWeight: "700",
-      }}
-    >
-      Types of Fencing Swords
-    </h1>
+
+    <div className="flex flex-row justify-center p-5 border-[#008CFF] border-4 bg-blue-800">
+      <h1 className="   text-white">
+        Types of Fencing Swords
+      </h1>
+    </div>
   );
 };
 
