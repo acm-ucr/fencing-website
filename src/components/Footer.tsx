6029d1719a00f24399f20429aa402609988319ae
Alton Qian <qian.alton@gmail.com>
2025-01-14 13:56:44 -0800

    Fixed the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,34 +8,32 @@ import Link from "../../public/Link.svg";
 const Footer = () => {
   return (
     <div className="relative">
-      <div className="absolute inset-0 flex flex-col items-center justify-center space-y-4 text-4xl font-bold right-2/3 text-white">
+      <div className="absolute inset-0 right-2/3 flex flex-col items-center justify-center space-y-4 text-4xl font-bold text-white">
         <>Fencing club</>
       </div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center top-20 right-2/3 text-white">
+      <div className="absolute inset-0 right-2/3 top-20 flex flex-col items-center justify-center text-white">
         <>University of California, Riverside</>
       </div>
-      <div className="absolute inset-0 flex flex-col justify-center left-3/4 text-white">
+      <div className="absolute inset-0 left-3/4 flex flex-col justify-center text-white">
         <>yourname@email.com</>
       </div>
-      <div className="absolute inset-0 flex flex-col justify-center top-16 left-3/4 text-white">
+      <div className="absolute inset-0 left-3/4 top-16 flex flex-col justify-center text-white">
         <>discord invite link</>
       </div>
-      <div className="absolute inset-0 flex flex-col justify-center top-32 left-3/4 text-white">
+      <div className="absolute inset-0 left-3/4 top-32 flex flex-col justify-center text-white">
         <>@instagram_handle</>
       </div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center top-32 left-2/4 right-12">
-        <Image src={Instagram} alt="insta logo" className=""/>
-
+      <div className="absolute inset-0 left-2/4 right-12 top-32 flex flex-col items-center justify-center">
+        <Image src={Instagram} alt="insta logo" className="" />
       </div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center left-2/4 right-12">
+      <div className="absolute inset-0 left-2/4 right-12 flex flex-col items-center justify-center">
         <Image src={mail} alt="mail logo" className="" />
       </div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center top-16 left-2/4 right-12">
+      <div className="absolute inset-0 left-2/4 right-12 top-16 flex flex-col items-center justify-center">
         <Image src={Link} alt="link logo" className="" />
       </div>
 
       <Image src={background} alt="page background" className="w-full" />
-
     </div>
   );
 };
