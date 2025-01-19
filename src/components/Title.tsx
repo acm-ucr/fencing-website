f700b30fdddc03425c7de66d6e47f0735c70782b
BenSan142 <pattyfstty@gmail.com>
2025-01-19 15:14:51 -0800

    Make text bigger

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <div className="flex justify-center items-start h-screen pt-32">
-      <h1 className="text-3xl font-bold text-black">
+      <h1 className="text-4xl font-bold text-black">
         Meet The Board
       </h1>
     </div>
