77d2e44daafd1d97d8534f29e911fb966b7baa66
BenSan142 <pattyfstty@gmail.com>
2025-01-19 15:30:20 -0800

    Fix Format

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const Title = () => {
   return (
-    <div className="flex justify-center items-start h-screen pt-32">
-      <h1 className="text-4xl font-bold text-black">
-        Meet The Board
-      </h1>
+    <div className="flex h-screen items-start justify-center pt-32">
+      <h1 className="text-4xl font-bold text-black">Meet The Board</h1>
     </div>
   );
 
