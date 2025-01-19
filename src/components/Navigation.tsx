a368d7bae5f5b8f13e2c4ba2f0fb5154dc01eb5c
ce <tom735593@gmail.com>
2025-01-18 20:57:45 -0800

    Simplified code by employing objects.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,41 +1,19 @@
 import React from "react";
 import Link from "next/link";
+import { ITEMS } from "@/data/navigation";
 
 const Navigation = () => {
   return (
     <>
       <div className="grid grid-cols-10 grid-rows-2 bg-black font-bold">
-        {/*black background is temp*/}
-        <Link
-          href="/"
-          className="col-start-2 col-end-4 row-start-2 text-2xl text-white"
-        >
-          Fencing Club
-        </Link>
-        <Link
-          href="/about"
-          className="col-start-6 row-start-2 flex justify-center text-2xl text-white"
-        >
-          About
-        </Link>
-        <Link
-          href="/board"
-          className="col-start-7 row-start-2 flex justify-center text-2xl text-white"
-        >
-          Board
-        </Link>
-        <Link
-          href="/events"
-          className="col-start-8 row-start-2 flex justify-center text-2xl text-white"
-        >
-          Events
-        </Link>
-        <Link
-          href="/join-us"
-          className="col-start-9 row-start-2 flex justify-center text-2xl text-white"
-        >
-          Join us
-        </Link>
+        {ITEMS.map((item) => (
+          <Link
+            href={`/${item.page}`}
+            className={`${item.col} ${item.cole} row-start-2 flex justify-center text-2xl text-white`}
+          >
+            {item.name}
+          </Link>
+        ))}
       </div>
     </>
   );
--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,1 +1,7 @@
-export const ITEMS = [];
+export const ITEMS = [
+  { name: "Fencing Club", page: "", col: "col-start-2", cole: "col-end-4 " },
+  { name: "about", page: "about", col: "col-start-6" },
+  { name: "board", page: "board", col: "col-start-7" },
+  { name: "events", page: "events", col: "col-start-8" },
+  { name: "join-us", page: "join-us", col: "col-start-9" },
+];
