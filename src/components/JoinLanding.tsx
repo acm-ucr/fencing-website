799135fbac71fb4d8c9761363078148474f8cc47
LawMot <molawr12@gmail.com>
2025-01-19 17:15:35 -0800

    updated Join Landing

--- src/components/JoinLanding.tsx
+++ src/components/JoinLanding.tsx
@@ -3,14 +3,14 @@ import React from "react";
 const JoinLanding = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-black p-10">
-      <div className="m-5 h-14 w-full rounded-lg bg-blue-500/60">
-        <div className="m-2 text-wrap text-center text-sm text-white">
+      <div className="mb-5 h-16 w-4/5 rounded-lg bg-blue-500/60">
+        <div className="text-wrap px-10 py-3 text-center text-sm text-white">
           We look forward to seeing you soon! We have practice every Monday and
           Wednesday 8-10PM!
         </div>
       </div>
 
-      <div className="h-28 w-full rounded-lg bg-gray-400"></div>
+      <div className="h-44 w-4/5 rounded-lg bg-gray-400"></div>
     </div>
   );
 };
