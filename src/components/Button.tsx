8c39be557290f4c8e50a681fc15cfb3a1655991f
Caly Carisse Cirilo <caly.cirilo@gmail.com>
2025-01-26 15:58:58 -0800

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,32 @@
-import React from "react";
-import Link from "next/link";
+// import React from "react";
+// import Link from "next/link";
 
-const Button = ({color = "bg-vex-black" }) => {
+// const Button = ({color = "bg-vex-black" }) => {
+//   return (
+//     <Link
+//       href={Link}
+//       className={`${color} text-white`}
+//     >
+//       {text}
+//     </Link>
+//   );
+// };
+
+// export default Button;
+
+import React from "react";
+import Link, { LinkProps } from "next/link";
+
+interface ButtonProps {
+  link: string;
+  text: string;
+  color?: string;
+}
+
+const Button = ({ color = "bg-vex-black", link, text }: ButtonProps) => {
   return (
     <Link
-      href={Link}
+      href={link as LinkProps["href"]}
       className={`${color} text-white`}
     >
       {text}
