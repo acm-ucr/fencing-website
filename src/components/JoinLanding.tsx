a2177d8e061b788b65c045c16d45253890a68bd3
LawMot <molawr12@gmail.com>
2025-01-27 11:21:08 -0800

    fix code formatting

--- src/components/JoinLanding.tsx
+++ src/components/JoinLanding.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const JoinLanding = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-black p-10 h-4/6">
+    <div className="flex h-4/6 flex-col items-center justify-center bg-black p-10">
       <div className="mb-5 h-1/6 w-4/5 rounded-lg bg-blue-500/60">
         <div className="text-wrap px-10 py-3 text-center text-sm text-white">
           We look forward to seeing you soon! We have practice every Monday and
@@ -10,8 +10,7 @@ const JoinLanding = () => {
         </div>
       </div>
 
-      <div className="h-3/6 w-4/5 rounded-lg bg-gray-400"> test
-      </div>
+      <div className="h-3/6 w-4/5 rounded-lg bg-gray-400"> test</div>
     </div>
   );
 };
