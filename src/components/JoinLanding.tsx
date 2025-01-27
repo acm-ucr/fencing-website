b9da7b8347cb5ecc64aad7eaa28e9d04b808612a
LawMot <molawr12@gmail.com>
2025-01-27 11:19:02 -0800

    update height in Join Landing

--- src/components/JoinLanding.tsx
+++ src/components/JoinLanding.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 const JoinLanding = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-black p-10">
-      <div className="mb-5 h-16 w-4/5 rounded-lg bg-blue-500/60">
+    <div className="flex flex-col items-center justify-center bg-black p-10 h-4/6">
+      <div className="mb-5 h-1/6 w-4/5 rounded-lg bg-blue-500/60">
         <div className="text-wrap px-10 py-3 text-center text-sm text-white">
           We look forward to seeing you soon! We have practice every Monday and
           Wednesday 8-10PM!
         </div>
       </div>
 
-      <div className="h-44 w-4/5 rounded-lg bg-gray-400"></div>
+      <div className="h-3/6 w-4/5 rounded-lg bg-gray-400"> test
+      </div>
     </div>
   );
 };
