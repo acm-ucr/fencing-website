4bbb073a27fa8d23125492c0ce998a8f2dab927e
LawMot <molawr12@gmail.com>
2025-01-27 12:19:43 -0800

    update bg-color

--- src/components/JoinLanding.tsx
+++ src/components/JoinLanding.tsx
@@ -3,14 +3,14 @@ import React from "react";
 const JoinLanding = () => {
   return (
     <div className="flex h-4/6 flex-col items-center justify-center bg-black p-10">
-      <div className="mb-5 h-1/6 w-4/5 rounded-lg bg-blue-500/60">
+      <div className="mb-5 h-1/6 w-4/5 rounded-lg bg-[#008CFF80]/50">
         <div className="text-wrap px-10 py-3 text-center text-sm text-white">
           We look forward to seeing you soon! We have practice every Monday and
           Wednesday 8-10PM!
         </div>
       </div>
 
-      <div className="h-3/6 w-4/5 rounded-lg bg-gray-400"> test</div>
+      <div className="h-3/6 w-4/5 rounded-lg bg-[#808080]"></div>
     </div>
   );
 };
