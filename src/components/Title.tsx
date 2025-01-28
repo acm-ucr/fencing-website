b0263f3830d287ef69affc1db0929158c3f4b2fe
av12122 <pv144155@gmail.com>
2025-01-28 11:24:35 -0800

    format

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,30 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 
 const Title = () => {
   return (
-
-    
-    <div className="flex flex-row justify-center items-center p-5 border-[#008CFF] border-2 bg-[#008CFF33] rounded-xl h-1 w-1/5 text-white font-bold">
-        Types of Fencing Swords
+    <div className="flex h-1 w-1/5 flex-row items-center justify-center rounded-xl border-2 border-[#008CFF] bg-[#008CFF33] p-5 font-bold text-white">
+      Types of Fencing Swords
     </div>
   );
 };
 
 export default Title;
-=======
-import React from "react";
-
-const Title = () => {
-  return (
-
-    <div className="flex flex-row justify-center p-5 border-[#008CFF] border-4 bg-blue-800">
-      <h1 className="   text-white">
-        Types of Fencing Swords
-      </h1>
-    </div>
-  );
-};
-
-export default Title;
->>>>>>> e9253d1 (testing git)
