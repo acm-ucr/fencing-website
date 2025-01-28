ace961e9aca53c49aa324a222b139151fbe357f9
av12122 <pv144155@gmail.com>
2025-01-28 11:20:18 -0800

    done?

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 
 const Title = () => {
@@ -11,3 +12,19 @@ const Title = () => {
 };
 
 export default Title;
+=======
+import React from "react";
+
+const Title = () => {
+  return (
+
+    <div className="flex flex-row justify-center p-5 border-[#008CFF] border-4 bg-blue-800">
+      <h1 className="   text-white">
+        Types of Fencing Swords
+      </h1>
+    </div>
+  );
+};
+
+export default Title;
+>>>>>>> e9253d1 (testing git)
