6b02047369dbb15084223c6941dd79b1320b5338
John Zhou <zhou9865@gmail.com>
2025-01-31 20:41:53 -0800

    turned it white

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = ({ text }: { text: string }) => {
   return (
     <div className="flex h-screen items-start justify-center pt-32">
-      <h1 className="text-4xl font-bold text-black">{text}</h1>
+      <h1 className="text-4xl font-bold text-white">{text}</h1>
     </div>
 
     // Change text to white once background is added
