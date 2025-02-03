b2fae936dca6ccb7754488ce0a8f2ff6332f4a94
ce <tom735593@gmail.com>
2025-02-03 12:35:54 -0800

    Added basic page prototype.

--- src/components/BoardCard.tsx
+++ src/components/BoardCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image, { StaticImageData } from "next/image";
+//import Image, { StaticImageData } from "next/image";
 
 // interface BoardCardProps {
 //   position: string;
@@ -12,12 +12,12 @@ const BoardCard = ({
   position,
   name,
   email,
-  image,
+  //image,
 }: {
   position: string;
   name: string;
   email: string;
-  image: StaticImageData;
+  //image: StaticImageData;
 }) => {
   return (
     <div className="board-card square-lg bg-gray-800 p-4 text-white shadow-lg">
@@ -25,11 +25,11 @@ const BoardCard = ({
         {position}
       </h3>
       <div className="board-card-image mb-4 flex justify-center">
-        <Image
+        {/*<Image
           className="h-40 w-40 rounded-lg object-cover"
-          src={image}
+          //src={image}
           alt="Picture of the author"
-        />
+        />*/}
       </div>
       <div className="text-center">
         <p className="board-card-name text-lg font-semibold">
