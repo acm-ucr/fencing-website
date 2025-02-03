425479b73a42066c3daa250afaf4b16ddb474782
ce <tom735593@gmail.com>
2025-02-03 12:37:12 -0800

    Added temp page

---
+++ src/app/board/page.tsx
@@ -0,0 +1,15 @@
+import Landing from "@/components/Landing";
+import Title from "@/components/Title";
+import BoardCard from "@/components/BoardCard";
+
+const page = () => {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <Landing />
+      <Title text="Meet The Board" />
+      <BoardCard name = "t" position="TT" email="@" />
+    </div>
+  );
+};
+
+export default page;
