caecb3b6eaa7afa91fdc6ed406748827ce410ee3
av12122 <pv144155@gmail.com>
2025-02-03 21:47:46 -0800

    title comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import About from "@/components/About";
-import Join from "@/components/Join";
-import Landing from "@/components/Landing";
-import What from "@/components/What";
-import Title from "@/components/Title";
-
-const Home = () => {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center bg-[#091422]">
-      <Landing />
-      <Title />
-      <About />
-      <What />
-      <Join />
-    </div>
-  );
-};
-
-export default Home;
-=======
 import About from "@/components/About";
 import Join from "@/components/Join";
 import Landing from "@/components/Landing";
@@ -38,4 +17,3 @@ const Home = () => {
 };
 
 export default Home;
->>>>>>> e9253d1 (testing git)
