25c0a844f5021074471d60475400cc214c1c10a5
LawMot <molawr12@gmail.com>
2025-02-03 19:21:19 -0800

    change 'Join Now!' from dev to button

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,9 @@ const Landing = () => {
       <div className="text-3lg text-wrap px-10 text-center italic text-white">
         Unversity of California, Riverside
       </div>
-      <div className="text-3lg my-4 text-wrap rounded-3xl border-2 border-[#008CFF] bg-[#008CFF80]/30 px-8 py-1 text-center font-semibold text-white/70">
+      <button className="text-3lg my-4 text-wrap rounded-3xl border-2 border-[#008CFF] bg-[#008CFF80]/30 px-8 py-1 text-center font-semibold text-white/70">
         Join Now!
-      </div>
+      </button>
     </div>
   );
 };
