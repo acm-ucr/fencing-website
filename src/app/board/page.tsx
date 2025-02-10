7a0004283d9414ff612f768c95528efa3d45ffb2
ce <tom735593@gmail.com>
2025-02-09 18:18:01 -0800

    Fixed positioning

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -5,9 +5,10 @@ import { ITEMS } from "@/data/board";
 
 const Page = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center mt-28" >
       <Landing />
       <Title text="Meet The Board" />
+      
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {ITEMS.map((item, index) => (
           <BoardCard
