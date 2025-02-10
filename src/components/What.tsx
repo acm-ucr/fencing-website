41db6e18ffe130d4aa91dbd779c11b6131820065
HMasroor3 <hoomanmasrour@gmail.com>
2025-02-10 13:11:13 -0800

    .

--- src/components/What.tsx
+++ src/components/What.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Image from "next/image";
+import link from "@/public/Link.svg";
 
 const What = () => {
-  return <div>What</div>;
+  return (
+    <div>
+      <div className="flex h-screen w-full items-end gap-[7.5%] p-[5%]">
+        <Image
+          alt="alttext"
+          src={link}
+          className="h-[68%] w-[90%] flex-1 overflow-hidden rounded-2xl bg-[grey]"
+        />
+        <div className="relative h-[62%] w-[10%] flex-[1.25] rounded-xl bg-[#054f8d]">
+          <div className="absolute left-[10%] top-[0%] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl border-4 border-[#0276d7] bg-[#082c4f] px-[8%] py-[3%]">
+            <h3 className="m-0 text-2xl font-bold text-white">What we do!</h3>
+          </div>
+          <p className="m-[10%] text-2xl text-white">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default What;
