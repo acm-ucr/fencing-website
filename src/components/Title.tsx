f781466317670caa32c432ee47319b5b10ba7825
ce <tom735593@gmail.com>
2025-02-10 12:41:58 -0800

    Fix build

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-
-const Title = ({ text }: { text: string }) => {
+type props = {
+  text: string;
+};
+const Title = ({ text }: props) => {
   return (
     <div className="flex h-screen items-start justify-center pt-32">
       <h1 className="text-4xl font-bold text-white">{text}</h1>
