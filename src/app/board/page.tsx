329aa8447dd4c751131db05b1f9bb7150c085b20
ce <tom735593@gmail.com>
2025-02-10 12:31:39 -0800

    Fix build

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -4,10 +4,11 @@ import BoardCard from "@/components/BoardCard";
 import { ITEMS } from "@/data/board";
 
 const Page = () => {
+  const titleName = { text: "Meet The Board" };
   return (
     <div className="mt-28 flex h-screen w-screen flex-col items-center justify-center">
       <Landing />
-      <Title text="Meet The Board" />
+      <Title {...titleName} />
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {ITEMS.map((item, index) => (
