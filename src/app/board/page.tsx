c0f0c3b42e467effc34af9c76586fc56906ab2e9
BenSan142 <pattyfstty@gmail.com>
2025-02-09 17:33:51 -0800

    Made Boxes for Board

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,15 +1,20 @@
 import Landing from "@/components/Landing";
 import Title from "@/components/Title";
 import BoardCard from "@/components/BoardCard";
+import { ITEMS } from "@/data/board";
 
-const page = () => {
+const Page = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <Landing />
       <Title text="Meet The Board" />
-      <BoardCard name="t" position="TT" email="@" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {ITEMS.map((item, index) => (
+          <BoardCard key={index} name={item.name} position={item.position} email={item.email} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
---
+++ src/data/board.ts
@@ -0,0 +1,12 @@
+export const ITEMS = [
+    { name: "Full Name", position: "Position Title",email: "contact@gmail.com", },
+    { name: "Full Name", position: "Position Title", email: "contact@gmail.com", },
+    { name: "Full Name", position: "Position Title", email: "contact@gmail.com", },
+    { name: "Full Name", position: "Position Title", email: "contact@gmail.com", },
+    { name: "Full Name", position: "Position Title", email: "contact@gmail.com", },
+    { name: "Full Name", position: "Position Title", email: "contact@gmail.com", },
+    { name: "Full Name", position: "Position Title", email: "contact@gmail.com", },
+    { name: "Full Name", position: "Position Title", email: "contact@gmail.com", },
+    { name: "Full Name", position: "Position Title", email: "contact@gmail.com", },
+  ];
+  
