65d42804d06d20aeac1ce86dbacb30f01f2541a9
BenSan142 <pattyfstty@gmail.com>
2025-02-10 12:26:17 -0800

    Removed comment

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -6,7 +6,7 @@ const Title = ({ text }: { text: string }) => {
       <h1 className="text-4xl font-bold text-white">{text}</h1>
     </div>
 
-    // Change text to white once background is added
+
   );
 };
 
