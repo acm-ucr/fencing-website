effa64754f992c5f2c60a70a8c3357216731dd60
CruzL092 <cruzbusiness760@gmail.com>
2025-02-16 18:31:21 -0800

    Applied Changes

--- src/components/What.tsx
+++ src/components/What.tsx
@@ -12,8 +12,8 @@ const What = () => {
           className="h-[68%] w-[90%] flex-1 overflow-hidden rounded-2xl bg-[grey]"
         />
         <div className="relative h-[62%] w-[10%] flex-[1.25] rounded-xl bg-[#054f8d]">
-          <div className="absolute left-[10%] top-[0%] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl border-4 border-[#0276d7] bg-[#082c4f] px-[8%] py-[3%]">
-            <h3 className="m-0 text-2xl font-bold text-white">What we do!</h3>
+          <div className="absolute left-[10%] top-[0%] -translate-y-1/2 transform rounded-2xl border-4 border-[#0276d7] bg-[#082c4f] px-[8%] py-[3%] md:-translate-x-16 lg:-translate-x-32">
+            <h3 className="m-0 text-3xl font-bold text-white">What we do!</h3>
           </div>
           <p className="m-[10%] text-2xl text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
