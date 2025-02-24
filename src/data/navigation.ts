6271abb04e3d0edba8d36d6929059ecab0d99415
sunbrandon <sunabrandon@gmail.com>
2025-02-23 23:35:30 -0800

    Fix 404

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -2,5 +2,5 @@ export const items = [
   { name: "About", link: "/" },
   { name: "Board", link: "/board" },
   { name: "Events", link: "/events" },
-  { name: "Join Us", link: "/join-us" },
+  { name: "Join Us", link: "/join" },
 ];
