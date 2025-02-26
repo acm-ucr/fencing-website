59842effa160d17934687852de914508f8828046
Caly C <caly.cirilo@gmail.com>
2025-02-25 21:14:31 -0800

    Moved div inside About Component + Changed About page.tsx

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -5,7 +5,7 @@ import Types from "@/components/Home/Types";
 
 const About = () => {
   return (
-    <div className="">
+    <div className="flex w-full flex-col items-center justify-center bg-fencing-overall-background">
       <Landing />
       <What />
       <Types />
