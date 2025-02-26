d9e60eda60a3a7aa212cf0d15c4ee2180cfca003
nidheesh-m-vakharia <nvakh003@ucr.edu>
2025-02-26 01:03:36 -0800

    format

--- src/components/events/gallery.tsx
+++ src/components/events/gallery.tsx
@@ -4,7 +4,9 @@ import { galleryItems } from "@/data/gallery";
 export default function Gallery() {
   return (
     <>
-      <p className="text-white mb-4 text-center text-2xl font-bold md:text-4xl">Photos</p>
+      <p className="mb-4 text-center text-2xl font-bold text-white md:text-4xl">
+        Photos
+      </p>
       <div className="mx-auto w-11/12 max-w-[1040px] columns-2 gap-2 space-y-2 md:columns-3 md:gap-4 md:space-y-4">
         {galleryItems.map((item, i) => (
           <Image
