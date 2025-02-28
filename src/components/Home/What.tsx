1ce4f87ab40ca0093ef2c4922f533d691457fc66
LawMot <molawr12@gmail.com>
2025-02-28 14:38:05 -0800

    Add what we do to home page

--- src/components/Home/What.tsx
+++ src/components/Home/What.tsx
@@ -9,15 +9,19 @@ const What = () => {
         /> */}
 
         <div className="h-[68%] w-[90%] flex-1 overflow-hidden rounded-2xl bg-[grey]"></div>
-        <div className="relative h-[62%] w-[10%] flex-[1.25] rounded-xl bg-[#054f8d]">
+        <div className="relative h-[68%] w-[10%] flex-[1.25] rounded-xl bg-[#054f8d]">
           <div className="absolute left-[10%] top-[0%] -translate-y-1/2 transform rounded-2xl border-4 border-[#0276d7] bg-[#082c4f] px-[8%] py-[3%] md:-translate-x-16 lg:-translate-x-32">
             <h3 className="m-0 text-3xl font-bold text-white">What we do!</h3>
           </div>
-          <p className="m-[10%] text-2xl text-white">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+          <p className="m-[8%] text-2xl text-white">
+            The UCR Fencing Club is a community for students who are passionate
+            about the sport of fencing. Whether you are a beginner or an
+            experienced fencer, we provide a welcoming environment for everyone
+            to learn, train, and compete. Our practices focus on technique,
+            strategy, and conditioning across all three disciplines: foil, épée,
+            and sabre. Our practices are at the SRC from Mondays 8:30pm -
+            10:30pm at MPR E and Wednesdays 8:30pm - 10:30pm at MPR A. Join us
+            to sharpen your skills and make new friends!
           </p>
         </div>
       </div>
