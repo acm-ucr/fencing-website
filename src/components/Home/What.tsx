b45262fcfd33b41d1fccbb27f4a83d79e831b519
LawMot <molawr12@gmail.com>
2025-03-03 12:52:54 -0800

    update local again

--- src/components/Home/What.tsx
+++ src/components/Home/What.tsx
@@ -1,6 +1,5 @@
 const What = () => {
   return (
-    <div>
       <div className="mb-12 flex h-screen w-full items-end gap-[7.5%] p-36">
         {/* <Image
           alt="alttext"
@@ -8,12 +7,13 @@ const What = () => {
           className="h-[68%] w-[90%] flex-1 overflow-hidden rounded-2xl bg-[grey]"
         /> */}
 
-        <div className="h-[68%] w-[90%] flex-1 overflow-hidden rounded-2xl bg-[grey]"></div>
-        <div className="relative h-[68%] w-[10%] flex-[1.25] rounded-xl bg-[#054f8d]">
-          <div className="absolute left-[10%] top-[0%] -translate-y-1/2 transform rounded-2xl border-4 border-[#0276d7] bg-[#082c4f] px-[8%] py-[3%] md:-translate-x-16 lg:-translate-x-32">
-            <h3 className="m-0 text-3xl font-bold text-white">What we do!</h3>
+        {/* <div> */}
+        <div className="w-9/10 h-2/3 flex-1 overflow-hidden rounded-2xl bg-[grey]"></div>
+        <div className="relative h-2/3 flex-[1.25] rounded-xl bg-[#054f8d]">
+          <div className="absolute left-20 -translate-y-1/2 transform rounded-2xl border-4 border-[#0276d7] bg-[#082c4f] px-[8%] py-[3%] md:-translate-x-16 lg:-translate-x-32">
+            <h3 className="text-3xl font-bold text-white">What we do!</h3>
           </div>
-          <p className="m-[8%] text-2xl text-white">
+          <p className="m-16 text-2xl text-white">
             The UCR Fencing Club is a community for students who are passionate
             about the sport of fencing. Whether you are a beginner or an
             experienced fencer, we provide a welcoming environment for everyone
@@ -25,7 +25,6 @@ const What = () => {
           </p>
         </div>
       </div>
-    </div>
   );
 };
 
