4aa9779dafd7d20a79525f3c996e1456a76908bd
av12122 <pv144155@gmail.com>
2025-03-22 23:36:31 -0700

    animated title text

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import background from "@/public/background.webp";
+import { motion } from "motion/react";
 
 type props = {
   text: string;
@@ -22,11 +23,18 @@ const Title = ({ text }: props) => {
           />
           <div className="absolute inset-0 bg-gradient-to-b from-transparent to-fencing-overall-background"></div>
         </div>
-        <div className="relative z-10 text-center">
-          <div className="px-10 py-2 pb-4 text-5xl font-bold text-white">
-            {text}
+        <motion.div
+          className="box"
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.2 }}
+        >
+          <div className="relative z-10 text-center">
+            <div className="px-10 py-2 pb-4 text-5xl font-bold text-white">
+              {text}
+            </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </>
   );
