f805eef2a6c2e97ba66f05ea958fe56388e3a5b9
Charleen Chen <charleenc485@gmail.com>
2025-03-23 00:37:30 -0700

    board animation

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import BoardCard from "@/components/Board/BoardCard";
 import { ITEMS } from "@/data/board";
 import Title from "../Title";
+import { motion } from "motion/react";
 
 function Board() {
   return (
@@ -10,12 +11,18 @@ function Board() {
       <Title text="Meet the Board" />
       <div className="grid w-9/12 grid-cols-1 gap-x-4 gap-y-12 md:grid-cols-2 md:gap-y-16 lg:grid-cols-3 lg:gap-y-20">
         {ITEMS.map((item, index) => (
-          <BoardCard
-            key={index}
-            name={item.name}
-            position={item.position}
-            image={item.image}
-          />
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <BoardCard
+              key={index}
+              name={item.name}
+              position={item.position}
+              image={item.image}
+            />
+          </motion.div>
         ))}
       </div>
     </div>
