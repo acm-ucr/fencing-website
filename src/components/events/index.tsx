6d42757f71c9101170dfdb3e19648d4622bd7fdf
John Zhou <zhou9865@gmail.com>
2025-03-27 02:15:47 -0700

    .

--- src/components/events/index.tsx
+++ src/components/events/index.tsx
@@ -20,7 +20,7 @@ import Title from "../Title";
 const Events = () => {
   const [current, setCurrent] = useState<EventProps>({});
 
-  const { isPending, error, data } = useQuery({
+  const { data } = useQuery({
     queryKey: ["repoData"],
     queryFn: async () => {
       const response =
